a41abc6fe97a7041000a77b3b5806bd8aed39c1f
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-13 20:54:35 -0400

    💄 feat: attributes

--- src/models/Db.ts
+++ src/models/Db.ts
@@ -13,3 +13,25 @@ export interface UsersAttributes {
   date_registration: Date;
   password: string;
 }
+
+export interface FlowAttributes {
+  id?: number;
+  userId: number;
+  code: string;
+  name: string;
+}
+export interface FlowNodesAttributes {
+  id?: number;
+  flowId: number;
+  position_x: number;
+  position_y: number;
+  type: string;
+  data: string;
+}
+
+export interface FlowEdgesAttributes {
+  id?: number;
+  flowId: number;
+  source: number;
+  target: number;
+}
