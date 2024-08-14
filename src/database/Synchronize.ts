6ae60ce54113c08a0db2ae47000e30455678f98b
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-13 20:55:54 -0400

    ✨ feat: flow

--- src/database/Synchronize.ts
+++ src/database/Synchronize.ts
@@ -1,12 +1,17 @@
 import { $log } from "@tsed/common";
-import { Users, NewPasswords } from "@app/database";
+import { Users, NewPasswords, Flow, FlowNodes, FlowEdges } from "@app/database";
 
 export async function synchronizeDB() {
   try {
     await Users.sync({ alter: true });
     await NewPasswords.sync({ alter: true });
+    await Flow.sync({ alter: true });
+    await FlowNodes.sync({ alter: true });
+    await FlowEdges.sync({ alter: true });
     $log.info("Done synchronize DB")
   } catch (error) {
     $log.error(error.message)
   }
 }
+
+synchronizeDB();
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,8 @@ export * from "./Connect";
 export * from "./NewPasswords";
 export * from "./Synchronize";
 export * from "./Users";
+export * from "./Flow";
+export * from "./FlowNodes";
+export * from "./FlowEdges";
 
- 
  
