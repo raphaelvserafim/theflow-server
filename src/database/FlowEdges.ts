b29dd780dee499dbccce80aa0ae06b495b1ed3cc
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-13 20:54:00 -0400

    ✨ feat: edges

---
+++ src/database/FlowEdges.ts
@@ -0,0 +1,60 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import { FlowEdgesAttributes } from '@app/models/Db';
+import { DB, Flow, FlowNodes } from "@app/database";
+
+class FlowEdges extends Model<FlowEdgesAttributes> implements FlowEdgesAttributes {
+  public id!: number;
+  public flowId!: number;
+  public source!: number;
+  public target!: number;
+
+  public static initialize(sequelize: Sequelize) {
+    FlowEdges.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        flowId: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          references: {
+            model: 'flow',
+            key: 'id',
+          },
+        },
+        source: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          references: {
+            model: FlowNodes,
+            key: 'id',
+          },
+        },
+        target: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          references: {
+            model: FlowNodes,
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'flow_edges',
+        timestamps: false,
+      }
+    );
+
+    FlowEdges.belongsTo(Flow, { foreignKey: 'flowId' });
+    FlowEdges.belongsTo(FlowNodes, { as: 'sourceNode', foreignKey: 'source' });
+    FlowEdges.belongsTo(FlowNodes, { as: 'targetNode', foreignKey: 'target' });
+  }
+}
+
+FlowEdges.initialize(DB.getInstance());
+
+export { FlowEdges };
