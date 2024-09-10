46c8e02bca8fb12c422bc556a64f99f3366fe6ee
Raphael Serafim <raphaelvserafim@gmail.com>
2024-09-10 13:46:17 -0400

    💄  feat: message type

--- src/models/Flow.ts
+++ src/models/Flow.ts
@@ -35,6 +35,10 @@ export class flowUpdateContentNodes {
   @Property()
   @Example("Oi")
   text_content?: string;
+  @Property()
+  @Example(false)
+  save_answer?: boolean;
+
 }
 
 export class flowConnectEdges {
@@ -44,4 +48,18 @@ export class flowConnectEdges {
   @Property()
   @Example("de8583-75a59f-656n57i1r8")
   target: string;
+}
+
+
+export enum MESSAGE_TYPE {
+  TEXT = "text_message",
+  IMAGE = "image_message",
+  MENU = "menu_message",
+  QUESTION = "question_message",
+  AUDIO = "audio_message",
+  DOCUMENT = "document_message",
+  AI = "ai_message",
+  API = "api_request",
+  END = "end",
+  START = "start",
 }
